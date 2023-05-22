eb7626a78155f3b7a88b516b3574f629c3f83fdc
harsha-1007 <harsha8331883221@gmail.com>
2023-05-22 20:09:23 +0530

    Harshavardhan changes

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -84,3 +84,122 @@ public class Advisor {
         this.email = email;
     }
 }
+
+@Entity
+public class Client{
+
+    @Id
+    @GeneratedValue()
+    private Long client_id;
+
+    @Column(nullable=false)
+    private String contact_information;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private String email_id;
+
+    @Column(nullable=false)
+    private Long advisorId;
+
+    protected Client( Long client_id,String contact_information,String name,String email_id,Long advisorId){
+        this.client_id = client_id;
+        this.contact_information = contact_information;
+        this.name = name;
+        this.email_id = email_id;
+    }
+
+    public Long getclientId(){
+        return client_id;
+    }
+
+    public void setcontactinfo(String contact_information){
+        this.contact_information=contact_information;
+    }
+
+    public String getcontactinfo(){
+        return contact_information;
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public String getname(){
+        return name;
+    }
+
+    public void setemail(String email_id){
+        this.email_id = email_id;
+    }
+
+    public String getemail(){
+        return email_id;
+    }
+}
+
+@Entity
+public class Security{
+
+    @Id
+    @GeneratedValue()
+    private Long Id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false)
+    private String category;
+
+    @Column(nullable=false)
+    private String purchaseDate;
+
+    @Column(nullable=false)
+    private double purchasePrice;
+
+    private Security(Long Id,String name,String category,String purchaseDate,double purchasePrice){
+        this.Id = Id;
+        this.name = name;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Long Id(){
+        return Id;
+    }
+
+    public void setname(String name){
+        this.name=name;
+    }
+
+    public String getname(){
+        return name;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void purchaseDate(String purchaseDate){
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getPurchasedate(){
+        return purchaseDate;
+    }
+
+    public void setPurchasePrice(double purchasePrice){
+        this.purchasePrice = purchasePrice;
+    }
+
+    public double getPurchasePrice(){
+        return purchasePrice;
+    }
+ }
